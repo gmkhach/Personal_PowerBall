c1b4c26b42a2505f7aa0f655ac6c9dd4232bb35b
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-21 22:24:18 -0400

    the rnd object is moved out of the loop

--- PowerBall/Form1.vb
+++ PowerBall/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class PowerBall
+    Dim rnd As New Random()
     Private Sub cmdGenerate_Click(sender As Object, e As EventArgs) Handles cmdGenerate.Click
-        Dim rnd As New Random()
+        Label1.Text = rnd.Next(69)
         Label2.Text = rnd.Next(69)
         Label3.Text = rnd.Next(69)
         Label4.Text = rnd.Next(69)
-        Label1.Text = rnd.Next(69)
         Label5.Text = rnd.Next(69)
         Label6.Text = rnd.Next(26)
     End Sub
